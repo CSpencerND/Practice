90650b85479c3733296ab9d54cab1dd19ebbc96d
cs <cspencernd@gmail.com>
2021-11-17 22:10:51 -0500

    miniLinter: good progress

--- JavaScript/codecademy/miniLinter.js
+++ JavaScript/codecademy/miniLinter.js
@@ -1,33 +1,73 @@
 let story = 'Last weekend, I took literally the most beautiful bike ride of my life. The route is called "The 9W to Nyack" and it actually stretches all the way from Riverside Park in Manhattan to South Nyack, New Jersey. It\'s really an adventure from beginning to end! It is a 48 mile loop and it basically took me an entire day. I stopped at Riverbank State Park to take some extremely artsy photos. It was a short stop, though, because I had a really long way left to go. After a quick photo op at the very popular Little Red Lighthouse, I began my trek across the George Washington Bridge into New Jersey.  The GW is actually very long - 4,760 feet! I was already very tired by the time I got to the other side.  An hour later, I reached Greenbrook Nature Sanctuary, an extremely beautiful park along the coast of the Hudson.  Something that was very surprising to me was that near the end of the route you actually cross back into New York! At this point, you are very close to the end.';
 
+
 let overusedWords = ['really', 'very', 'basically'];
 let unnecessaryWords = ['extremely', 'literally', 'actually' ];
 let storyWords = story.split(' ');
 
+
 // Filters out unnecessaryWords from storyWords
-let betterWords = storyWords.filter(
+const betterWords = storyWords.filter(
     word => !unnecessaryWords.includes(word)
 );
 
+
 // Counts occurance of overusedWords in betterWords
 const countOverused = overusedWords.map(oWord => {
-    const wordCount = betterWords.reduce((count, word) => {
-        if (word === oWord) count ++;
+    const wordCount = betterWords.reduce((count, bWord) => {
+        if (bWord === oWord) count ++;
         return count;
     }, 0);
     let result = {};
     result[oWord] = wordCount;
     return result;
-})
+}); const overusedCount = Object.assign({}, ...countOverused);
+
 
 // Counts the number of sentences in betterWords
 const countSentences = betterWords.reduce((count, word) => {
     if (word.includes(".") || word.includes("!")) count++;
     return count;
-}, 0)
+}, 0);
+
+
+// Remove every other occurance of each overused world
+const removeEveryOther = () => {
+    for (let i=0; i < overusedWords.length; i++) 
+    {
+        let count = 0;
+        for (let j=0; j < betterWords.length; j++)
+        {
+            if (overusedWords[i] === betterWords[j]) {
+                count++;
+                if (count % 2 === 0) {
+                    betterWords.splice(j, 1);
+                }
+            }
+        }
+    }
+}; removeEveryOther();
+
+
+// Count occurences of a word in array
+const countOccurences = (word) => {
+    const wordCount = betterWords.reduce((count, bWord) => {
+        if (bWord === word) count++;
+        return count;
+    }, 0);
+    let result = {};
+    result[word] = wordCount;
+    return result;
+}
+// Get occurences of every word
+const allOccurences = betterWords.map(word => countOccurences(word));
+
+const highestOccurence = allOccurences.reduce(function(prev, current) {
+    return (prev.y > current.y) ? prev : current
+}) //returns object
 
 
 // console.log(storyWords.length);
 // console.log(countSentences)
-// console.log(countOverused);
-console.log(betterWords.join(" "))
+// console.log(overusedCount);
+// console.log(betterWords.join(" "), "\n");
