d4b91bf26f5eabaf3beffede38cf2749cf37263d
cs <cspencernd@gmail.com>
2021-12-16 13:53:07 -0500

    add inRange

--- webDev/lodashProject/_.js
+++ webDev/lodashProject/_.js
@@ -1,10 +1,29 @@
 const _ = {
+
     clamp(number, lower, upper)
     {
         const lowerClampedValue = Math.max(number, lower);
         const clampedValue = Math.min(lowerClampedValue, upper);
         return clampedValue;
     },
+
+    inRange(number, start, end=undefined)
+    {
+        if (end === undefined)
+        {
+            end = start;
+            start = 0;
+        }
+
+        if (start > end)
+        {
+            let temp = start;
+            start = end; end = temp; delete temp;
+        }
+
+        const isInRange = (number >= start && number < end);
+        return isInRange;
+    }
 
 };
 
