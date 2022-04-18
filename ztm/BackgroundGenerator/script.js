944bce3ea5e5cab91ba4b6ac5b109c5787c5840b
cs <cspencernd@gmail.com>
2022-04-18 11:47:27 -0400

    messing with npm

--- ztm/BackgroundGenerator/script.js
+++ ztm/BackgroundGenerator/script.js
@@ -1,3 +1,8 @@
+const _ = require('lodash')
+
+const arr = [1,2,3,4,5,6,7,8]
+console.log('answer', _.without(arr, 3))
+
 const css = document.querySelector('h3')
 const color1 = document.querySelector('.color1')
 const color2 = document.querySelector('.color2')
