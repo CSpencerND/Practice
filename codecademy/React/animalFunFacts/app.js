7d942ec29999064ecc7914c38601792ed73da031
cs <cspencernd@gmail.com>
2022-05-12 21:04:02 -0400

    init React dir

---
+++ codecademy/React/animalFunFacts/app.js
@@ -0,0 +1,44 @@
+import { animals } from './animals'
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+const showBackground = true
+
+const background = (
+    <img className="background" alt="ocean" src="images/ocean.jpg" />
+)
+
+const images = []
+for (const animal in animals) {
+    images.push(
+        <img
+            key={animal}
+            className="animal"
+            alt={animal}
+            src={animals[animal].image}
+            ariaLabel={animal}
+            role="button"
+            onClick={displayFact}
+        />
+    )
+}
+
+function displayFact(e) {
+    const animalName = e.target.alt
+    const animalInfo = animals[animalName]
+    const randomIndex = Math.floor(Math.random() * animalInfo.facts.length)
+    const funFact = animalInfo.facts[randomIndex]
+    document.getElementById('fact').innerHTML = funFact
+}
+
+const title = ''
+const animalFacts = (
+    <div>
+        <h1>{title === '' ? 'Click an animal for a fun fact' : title}</h1>
+        {showBackground && background}
+        <div className="animals">{images}</div>
+        <p id="fact"></p>
+    </div>
+)
+
+ReactDOM.render(animalFacts, document.getElementById('root'))
