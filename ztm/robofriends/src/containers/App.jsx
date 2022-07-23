e768494df30c158ee67d42123dfa0a843689bf23
cs <cspencernd@gmail.com>
2022-07-23 03:58:55 -0400

    add error boundry and deploy

--- ztm/robofriends/src/containers/App.jsx
+++ ztm/robofriends/src/containers/App.jsx
@@ -1,15 +1,16 @@
-import React, { Component } from 'react'
-import CardList from '../components/CardList'
-import SearchBox from '../components/SearchBox'
-import Scroll from '../components/Scroll'
-import './App.css'
+import React, { Component } from "react"
+import CardList from "../components/CardList"
+import SearchBox from "../components/SearchBox"
+import Scroll from "../components/Scroll"
+import ErrorBoundry from "../components/ErrorBoundry"
+import "./App.css"
 
 class App extends Component {
     constructor() {
         super()
         this.state = {
             robots: [],
-            searchField: '',
+            searchField: "",
         }
     }
 
@@ -31,32 +32,33 @@ class App extends Component {
                 <h1 className="f1 ">RoboFriends</h1>
                 <SearchBox searchChange={this.onSearchChange} />
                 <Scroll>
-                    <CardList robots={filteredRobots} />
+                    <ErrorBoundry>
+                        <CardList robots={filteredRobots} />
+                    </ErrorBoundry>
                 </Scroll>
             </main>
         )
     }
 
     componentDidMount() {
-        fetch('https://jsonplaceholder.typicode.com/users')
-            .then(res => {
+        fetch("https://jsonplaceholder.typicode.com/users")
+            .then((res) => {
                 if (res.ok) {
                     return res.json()
                 }
                 return Promise.reject(res)
             })
             .then((users) => this.setState({ robots: users }))
-            .catch(e => alert('Something went wrong', e))
+            .catch((e) => alert("Something went wrong", e))
 
-    // async componentDidMount() {
-    //     const res =
-    //         (await fetch('https://jsonplaceholder.typicode.com/users')) ||
-    //         fetch('https://jsonplaceholder.cypress.io/users')
-    //     if (res.ok) {
-    //         const json = await res.json()
-    //         this.setState({ robots: json })
-    //     }
-
+        // async componentDidMount() {
+        //     const res =
+        //         (await fetch('https://jsonplaceholder.typicode.com/users')) ||
+        //         fetch('https://jsonplaceholder.cypress.io/users')
+        //     if (res.ok) {
+        //         const json = await res.json()
+        //         this.setState({ robots: json })
+        //     }
     }
 }
 
