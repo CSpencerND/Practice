15b23538de195c7daa2ba2cf63b7414a276671c3
cspencer <cspencernd@gmail.com>
2022-11-22 11:25:44 -0500

    change port

--- testServer/server/app.js
+++ testServer/server/app.js
@@ -5,7 +5,7 @@ const process = require("process")
 const dotenv = require("dotenv")
 
 dotenv.config()
-const PORT = process.env.PORT
+const PORT = process.env.PORT || 3001
 
 const app = express()
 
